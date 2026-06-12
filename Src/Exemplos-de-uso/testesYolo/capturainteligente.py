8508a638fad9185a6c74a221f7da7e8500f08465
Augusto Freitas <162934392+IgutoI@users.noreply.github.com>
2026-06-12 06:57:17 -0700

    ATUALIZADO, SE TUDO DER CERTO DESSA VEZ SUBIU DIREITO

--- Src/Exemplos-de-uso/testesYolo/capturainteligente.py
+++ Src/Exemplos-de-uso/testesYolo/capturainteligente.py
@@ -1,34 +1,130 @@
 import cv2
-from ultralytics import YOLO
+import numpy as np
+import time
+import os
 
-# Configurações
-#stream_url = "http://192.168.18.103:8080/video"#Ajustar o IP para cada iteração
-modelo = YOLO("yolov8n.pt")  # Modelo nano
-frame_skip = 2  # Pula frames
-frame_count = 0
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
-# # Abre o stream
-# cap = cv2.VideoCapture(stream_url, cv2.CAP_FFMPEG)
-cap = cv2.VideoCapture(0, cv2.CAP_FFMPEG)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+prototxt_path = os.path.join(base_dir, "deploy.prototxt")
+model_path = os.path.join(base_dir, "res10_300x300_ssd_iter_140000_fp16.caffemodel")
+
+print(prototxt_path)
+print(model_path)
+
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+# Modelo OpenCV DNN
+#net = cv2.dnn.readNetFromCaffe(
+#    "res10_300x300_ssd_iter_140000_fp16.caffemodel"
+#)
+
+cap = cv2.VideoCapture(0)
+
+foto_salva = False
+ultimo_tempo = time.time()
 
 while True:
+
     ret, frame = cap.read()
+
     if not ret:
         break
 
-    frame_count += 1
-    if frame_count % frame_skip != 0:
-        continue
+    h, w = frame.shape[:2]
 
-    # Detecção 2 Para carros, 11 para placa de pare
-    resultados = modelo(frame, classes=[11], show=False)
-    frame_processado = resultados[0].plot()
+    # Blob para o detector
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(frame, (300, 300)),
+        1.0,
+        (300, 300),
+        (104.0, 177.0, 123.0)
+    )
 
-    # Exibe
-    cv2.imshow("YOLO + Camera IP", frame_processado)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    net.setInput(blob)
+    detections = net.forward()
+
+    melhor_face = None
+    melhor_conf = 0
+
+    for i in range(detections.shape[2]):
+
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.7:
+
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+
+            startX, startY, endX, endY = box.astype("int")
+
+            face = frame[startY:endY, startX:endX]
+
+            if face.size == 0:
+                continue
+
+            face_w = endX - startX
+            face_h = endY - startY
+
+            area = face_w * face_h
+
+            # pega melhor rosto
+            if confidence > melhor_conf:
+                melhor_conf = confidence
+                melhor_face = (
+                    startX, startY,
+                    endX, endY,
+                    face,
+                    area
+                )
+
+    if melhor_face is not None:
+
+        startX, startY, endX, endY, face, area = melhor_face
+
+        # brilho médio
+        gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        sharpness = cv2.Laplacian(gray, cv2.CV_64F).var()
+        brilho = np.mean(gray)
+
+        # desenha info
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0,255,0), 2)
+
+        texto = f"Conf:{melhor_conf:.2f} Bright:{brilho:.0f} Sharp:{sharpness:.0f}"
+
+        cv2.putText(
+            frame,
+            texto,
+            (startX, startY - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.6,
+            (0,255,0),
+            2
+        )
+
+        # critérios mínimos
+        rosto_bom = (
+            area > 50000 and
+            brilho > 80 and
+            brilho < 200 and
+            melhor_conf > 0.90 and
+            sharpness >120
+        )
+
+        if rosto_bom and not foto_salva:
+
+            caminho = r"C:\Users\AUGUSTO\Desktop\SistDistrib-main\SistDistrib-main\Src\Exemplos-de-uso\testesYolo\images\testefotowebcam.jpg"
+
+            cv2.imwrite(caminho, face)
+
+            print("FOTO CAPTURADA!")
+
+            foto_salva = True
+
+            time.sleep(1)
+
+            break
+
+    cv2.imshow("Captura Inteligente", frame)
+
+    if cv2.waitKey(1) == 27:
         break
 
 cap.release()
